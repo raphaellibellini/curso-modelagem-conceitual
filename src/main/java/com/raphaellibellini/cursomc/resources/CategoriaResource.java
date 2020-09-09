db5b49dd277c06fce76d13625d2c8c0684900f34
Raphaelli Bellini <raphaelli.bellini@ntendencia.com.br>
2020-09-09 15:58:29 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/raphaellibellini/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/raphaellibellini/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.raphaellibellini.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.raphaellibellini.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias") //endpoint
@@ -10,7 +15,15 @@ public class CategoriaResource {
 	
 	//é importante atribuir os métodos REST para cada método
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
